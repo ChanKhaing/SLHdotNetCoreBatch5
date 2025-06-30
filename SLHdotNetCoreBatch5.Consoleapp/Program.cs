7bde3733479223a9a35dcc39de08beda11b88c6f
User <User@DESKTOP-878Q29H>
2025-07-01 01:31:21 +0630

    Add project files.

---
+++ SLHdotNetCoreBatch5.Consoleapp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is a .NET 8.0 console application using C# 12.0 language features.");
+Console.ReadLine();
