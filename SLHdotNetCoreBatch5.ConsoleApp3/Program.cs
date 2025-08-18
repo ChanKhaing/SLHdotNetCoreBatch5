cf731c0a323463e67d20faf87196a9394cb3f3aa
ChanKhaing <chankhinem@gmail.com>
2025-08-18 14:01:55 +0630

    feat:database first

---
+++ SLHdotNetCoreBatch5.ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SLHdotNetCoreBatch5.database.Models;
+
+Console.WriteLine("Hello, World!");
+AppDbContext db = new AppDbContext();
+   var lst          = db.TblBlogs.Where(x => x.DeleteFlag == false).ToList();
+foreach (var blog in lst)
+{
+    Console.WriteLine(blog.BlogId);
+    Console.WriteLine(blog.BlogTitle);
+    Console.WriteLine(blog.BlogAuthor);
+    Console.WriteLine(blog.BlogContent);
+}
