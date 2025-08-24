7e62af7715ff792ecf264a23afcd3c9ad6682806
ChanKhaing <chankhinem@gmail.com>
2025-08-24 23:52:30 +0630

    dapper service

---
+++ SLHdotNetCoreBatch5.ConsoleApp3/Dapperservice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLHdotNetCoreBatch5.ConsoleApp3
+{
+    internal class Dapperservice
+    {
+    }
+}
